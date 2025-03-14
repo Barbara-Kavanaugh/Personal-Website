344a24ab9e9e8e32491233f4e7e90ae50f1a63d1
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2025-03-13 20:34:38 -0400

    started navbar

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import "./index.css"
 import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/Navbar";
 import { useState } from "react";
 
 function App() {
@@ -8,7 +9,14 @@ function App() {
 
   return (
     <>
-      {!isLoading && <LoadingScreen onComplete={() => setIsLoading(true)} />}
+      {!isLoading && <LoadingScreen onComplete={() => setIsLoading(true)} />}{" "}
+      <div 
+        className={`min-h-screen transition-opacity duration-700 
+          ${isLoading ? "opacity-100" : "opacity-0"} 
+          bg-black text-gray-100`}
+      >
+        <Navbar />
+      </div>
     </>
   );
 }
