8df9c1fb1c91dc8b9b1f267fb01206cb28272469
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2025-03-15 11:06:28 -0400

    styling changes & about me section

--- personal-website/src/components/Navbar.jsx
+++ personal-website/src/components/Navbar.jsx
@@ -7,22 +7,22 @@ export const Navbar = ( {menuOpen, setMenuOpen} ) => {
     }, [menuOpen]);
 
     return (
-        <nav className="fixed top-0 w-full z-40 bg-[rgba(241, 231, 231, 0.8) bg-transparent backdrop-blur-5xl border-white/10 shadow-lg">
+        <nav className="fixed top-0 w-full z-40 bg-[#fa3535] backdrop-blur-5xl border-white/10 shadow-lg">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
-                    <a href="#home" className="text-xl font-bold text-[#000000] font-['Gelasio']">
-                        ༺ ♱ <span className="text-[#000000]">Barbara</span> ♱ ༻
+                    <a href="#home" className="text-xl font-bold text-[#ffffff] hover:text-[#FBAAAA] transition-colors font-['Gelasio']">
+                        ༺ ♱ Barbara ♱ ༻
                     </a>
 
-                    <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden text-[#000000]" onClick={() => setMenuOpen((prev) => !prev)}>
+                    <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden text-[#ffffff]" onClick={() => setMenuOpen((prev) => !prev)}>
                         <GiHamburgerMenu />
                     </div>
 
                     <div className="hidden md:flex items-center space-x-8 font-['Gelasio']">
-                        <a href="#home" className="font-['Gelasio'] font-bold text-[#000000] hover:text-[#FBAAAA] transition-colors">Home</a>
-                        <a href="#about" className="font-['Gelasio'] font-bold text-[#000000] hover:text-[#FBAAAA] transition-colors">About Me</a>
-                        <a href="#projects" className="font-['Gelasio'] font-bold text-[#000000] hover:text-[#FBAAAA] transition-colors">Projects</a>
-                        <a href="#contact" className="font-['Gelasio'] font-bold text-[#000000] hover:text-[#FBAAAA] transition-colors">Contact Me</a>
+                        <a href="#home" className="font-['Gelasio'] font-bold text-[#ffffff] hover:text-[#FBAAAA] transition-colors">Home</a>
+                        <a href="#about" className="font-['Gelasio'] font-bold text-[#ffffff] hover:text-[#FBAAAA] transition-colors">About Me</a>
+                        <a href="#projects" className="font-['Gelasio'] font-bold text-[#ffffff] hover:text-[#FBAAAA] transition-colors">Projects</a>
+                        <a href="#contact" className="font-['Gelasio'] font-bold text-[#ffffff] hover:text-[#FBAAAA] transition-colors">Contact Me</a>
                     </div>
                 </div>
             </div>
--- personal-website/src/components/sections/Home.jsx
+++ personal-website/src/components/sections/Home.jsx
@@ -4,10 +4,21 @@ export const Home = () => {
             <div className="text-center z-10 px-4">
                 <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r leading-right from-black to-[#9B0808] bg-clip-text text-transparent">Hello, I'm Barbara!</h1>
                 <p className="text-black text-lg mb-8 max-w-lg mx-auto">
-                    I am an analytical and results-driven data analyst with a solid foundation in computer science and a passion for transforming data into actionable insights. With experience spanning multiple industries, including insurance and construction, I specialize in leveraging a variety of data tools and technologies—such as SQL, Power BI, Python, and Oracle—to enhance decision-making, optimize processes, and support data-driven strategies.
+                    I am a data analyst with a computer science background, passionate about turning data into actionable insights. Experienced in insurance and construction, I use tools like SQL, Power BI, Python, and Oracle to improve decision-making and processes. I am also active in the Maryvale Preparatory School Alumnae Association, supporting curriculum and community initiatives.
                 </p>
                 <div className="flex justify-center space-x-4">
-                    <a href="/src/assets/Barbara_Kavanaugh_Resume_2025.pdf" target="_blank" rel="noopener noreferrer">View Resume</a>
+                    <a 
+                        className="bg-[#fa3535] text-white py-3 px-6 rounded font-medium transition relative 
+                        overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(246, 59, 59, 0.4)]" 
+                        href="/src/assets/Barbara_Kavanaugh_Resume_2025.pdf" 
+                        target="_blank" rel="noopener noreferrer">
+                        View Resume
+                    </a>
+                    <a 
+                        className="border border-[#9B0808] text-[#9B0808] py-3 px-6 rounded font-medium transition-all duration-200 overflow-hidden hover:-translate-y-0.5 
+                        hover:shadow-[0_0_15px_rgba(246, 59, 59, 0.2)] hover:bg-[#FBAAAA]" href="#contact">
+                        Contact Me
+                    </a>
                 </div>
             </div>
         </section>
